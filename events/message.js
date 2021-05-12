96a6451bf0b655a03df2bf19872dcf57562d6a20
Tyler Campbell <007youdontknowme@gmail.com>
2021-05-12 19:09:17 -0400

    added events folder,events\message.js , events\ready.js and packages.json

---
+++ events/message.js
@@ -0,0 +1,17 @@
+module.exports = (clinet, messae) => {
+    if(MessageChannel.author.bot) return;
+
+
+
+    if(message.content.indexof(client.config.prefix) !==0) return;
+
+
+    const args = message.content.slice(client.config.prefix.lenght).trim().split(/ +/g);
+    const command = args.shift().toLowerCase():
+
+    const cmd = client.commands.get(command);
+
+    if (!cmd) return;
+
+    cmd.run(client, message, args);
+};
---
+++ events/ready.js
@@ -0,0 +1,9 @@
+module.exports = (clients) => {
+    console.log('Im alive as ' + clients.user.tag)
+
+    const activities = [`minecraft in Homers basement`]
+
+    serInterval(() => {
+        let activity = activities[Math.floor(Math.random() * activities.length)]
+        client.user.setActivity(activity, {type: "playing"})
+}
