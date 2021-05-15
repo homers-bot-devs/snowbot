926d3e979333036598d4c039a39bbca0dd9ad269
RaspberryRBX <61115942+RaspberryRBX@users.noreply.github.com>
2021-05-15 19:08:25 +0100

    Update about.js

--- commands/about.js
+++ commands/about.js
@@ -3,9 +3,9 @@ const { MessageEmbed } = require("discord.js");
 
 exports.run = async(client, message, args) => {
   let embed = new MessageEmbed()
-  .addField('About Me', 'im a disocrd bot made buy Nunya-9018/homersrebuttal#9018')
-  .addField(`${config.prefix}` + 'help','if you need help with commands or want to know what they do ' + `${config.prefix}` + 'help')
-  .addField('Version', 'The current verison of the bot is 1.0')
+  .addField('About Me', 'Im A Discord Bot Made By Nunya-9018/homersrebuttal#9018')
+  .addField(`${config.prefix}` + 'help','If You Need Help With Commands Or Want To Know What They Do ' + `${config.prefix}` + 'help')
+  .addField('Version', 'The Current Verison Of The Bot Is 1.0')
       .setColor("GREEN")
       .setTimestamp()
       .setFooter(`${message.author.tag}`, message.author.displayAvatarURL())
