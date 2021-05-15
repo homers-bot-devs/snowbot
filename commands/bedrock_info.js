0a33f918bc849424c4a1d4c025fe688f80c675f6
Tyler Campbell <007youdontknowme@gmail.com>
2021-05-14 20:03:47 -0400

    Create bedrock_info.js

---
+++ commands/bedrock_info.js
@@ -0,0 +1,10 @@
+const config = require('../config')
+const { MessageEmbed} = require("discord.js");
+exports.run = async(client, message, args) => {
+    let embed = new MessageEmbed()
+    .setTitle('Bedrock_info')
+    .setDescription('The bedrock ip and port info')
+    .addField('The server name', 'snowskingdom')
+    .addField('IP', 'The bedrock IP is:')
+    .addField('Port', 'The bedrock Port is:')
+}
