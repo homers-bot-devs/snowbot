54b683e6f9bc8e0e7b7926a55f1a74a78d17d11f
Tyler Campbell <007youdontknowme@gmail.com>
2021-05-15 14:15:31 -0400

    Update about.js

--- commands/about.js
+++ commands/about.js
@@ -3,7 +3,7 @@ const { MessageEmbed } = require("discord.js");
 
 exports.run = async(client, message, args) => {
   let embed = new MessageEmbed()
-  .addField('About Me', 'Im A Discord Bot Made By Nunya-9018/homersrebuttal#9018')
+  .addField('About Me', 'Im A Discord Bot Made By Nunya-9018/HomersRebuttal#9018')
   .addField(`${config.prefix}` + 'help','If You Need Help With Commands Or Want To Know What They Do ' + `${config.prefix}` + 'help')
   .addField('Version', 'The Current Verison Of The Bot Is 1.0')
       .setColor("GREEN")
