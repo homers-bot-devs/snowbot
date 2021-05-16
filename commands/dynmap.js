e9b9971eccf1a1b957859c841c40bab1184edea9
Tyler Campbell <007youdontknowme@gmail.com>
2021-05-16 14:57:49 -0400

    Update dynmap.js

--- commands/dynmap.js
+++ commands/dynmap.js
@@ -5,4 +5,8 @@ exports.run = async(client, message, args) => {
     .setTitle('Dynmap')
     .setDescription('The url and information to use snowskingdom dynamic map system plugin or dynmap')
     .addField('url','put url here')
-}
+    .setColor("GREEN")
+    .setTimestamp()
+    .setFooter(`${message.author.tag}`, message.author.displayAvatarURL())
+     message.channel.send(embed)
+    }
